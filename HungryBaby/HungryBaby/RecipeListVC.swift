d4458de35a4b2fa2a133d19db7a18c1b9a07d281
qwertyshan <rao.shantanu@gmail.com>
2016-02-25 15:37:10 -0800

    Add: Tab view controller on Storyboard

---
+++ HungryBaby/HungryBaby/RecipeListVC.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeListVC.swift
+//  HungryBaby
+//
+//  Created by Shantanu Rao on 2/25/16.
+//  Copyright © 2016 Shantanu Rao. All rights reserved.
+//
+
+import UIKit
+
+class RecipeListVC: UIViewController {
+
+}
