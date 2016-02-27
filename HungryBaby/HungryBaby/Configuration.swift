ca375d778afd7ee1d24e13754e5853de24945f15
qwertyshan <rao.shantanu@gmail.com>
2016-02-26 16:42:26 -0800

    Add: Model for Configuration

--- HungryBaby/HungryBaby/Configuration.swift
+++ HungryBaby/HungryBaby/Configuration.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UIKit
+
+class Configuration: NSObject {
+
+    struct Allergies {
+        var peanuts: Bool?
+        var garlic: Bool?
+    }
+    
+    enum DietaryPreference {
+        case Vegan, Vegetarian, OvoLactoVegetarian, None
+    }
+    
+    var name: String?
+    var image: UIImage?
+    var dateOfBirth: NSDate?
+    var allergies: Allergies
+    var dietaryPreference: DietaryPreference
+    
+    override init() {
+        self.name = nil
+        self.image = nil
+        self.dateOfBirth = nil
+        self.allergies = Allergies(peanuts: false, garlic: false)
+        self.dietaryPreference = DietaryPreference.None
+    }
+    
+}
