7a62570747fb40d1d01da7783a73c97f9498fa68
qwertyshan <rao.shantanu@gmail.com>
2016-02-28 14:05:19 -0800

    Add: Login view and VC

--- HungryBaby/HungryBaby/LoginVC.swift
+++ HungryBaby/HungryBaby/LoginVC.swift
@@ -7,6 +7,35 @@
 //
 
 import Foundation
-import Firebase
+import UIKit
 
-
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginWithEmail: UIButton!
+    @IBOutlet weak var loginAnonymously: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("debug in LoginVC")
+        getDataOnLogin()
+        
+    }
+    
+    func getDataOnLogin() {
+        APIClient.sharedInstance().getRecipePackage({data, error in
+            print("debug2 in LoginVC")
+            
+            if (error != nil) {
+                print("debug3 in LoginVC")
+                
+                print(error)
+            } else {
+                print("debug4 in LoginVC")
+                
+                print(data)
+            }
+        })
+    }
+}
