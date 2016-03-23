b3c0ef9d7af078f4ea8d6f1bc80d8576f11e5d4d
qwertyshan <rao.shantanu@gmail.com>
2016-03-22 17:12:10 -0700

    Add: Storyboard scene for Recipe Details

--- HungryBaby/HungryBaby/RecipeCell.swift
+++ HungryBaby/HungryBaby/RecipeCell.swift
@@ -15,3 +15,36 @@ class RecipeCell: UITableViewCell {
     @IBOutlet weak var favImage: UIImageView!
     
 }
+
+class RecipeHeader: UITableViewCell {
+    @IBOutlet var recipeImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+}
+
+class RecipeSummary: UITableViewCell {
+    @IBOutlet var summaryLabel: UILabel!
+}
+
+class RecipeNumbers: UITableViewCell {
+    @IBOutlet var prepLabel: UILabel!
+    @IBOutlet var cookLabel: UILabel!
+    @IBOutlet var forAgesLabel: UILabel!
+    @IBOutlet var makesLabel: UILabel!
+}
+
+class RecipeIngredients: UITableViewCell {
+    @IBOutlet var ingredientLabel: UILabel!
+}
+
+class RecipeMethod: UITableViewCell {
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var methodStepLabel: UILabel!
+}
+
+class RecipeNutrition: UITableViewCell {
+    @IBOutlet var carbsLabel: UILabel!
+    @IBOutlet var fatsLabel: UILabel!
+    @IBOutlet var proteinsLabel: UILabel!
+    @IBOutlet var caloriesLabel: UILabel!
+}
+
--- HungryBaby/HungryBaby/RecipeListVC.swift
+++ HungryBaby/HungryBaby/RecipeListVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 
+//@IBDesignable
+
 class RecipeListVC: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
     
     // MARK: - Properties
@@ -93,8 +95,8 @@ class RecipeListVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         if segue.identifier == "RecipeDetailSegue" {
             if let detailController = segue.destinationViewController as? RecipeDetailVC {
                 if let indexPath = tableView.indexPathForSelectedRow {
-                    detailController.recipe = fetchedResultsController.objectAtIndexPath(indexPath) as! Recipe
-                    detailController.recipeIndex = indexPath.row
+                    //detailController.recipe = fetchedResultsController.objectAtIndexPath(indexPath) as! Recipe
+                    //detailController.recipeIndex = indexPath.row
                 }
             }
         }
